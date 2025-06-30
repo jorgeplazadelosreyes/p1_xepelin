05d187c94b90bb3f4dae6a19bad655779b2a798b
Jorge Plaza de los Reyes <jplaza@buk.cl>
2025-07-01 01:02:02 +0200

    feat: add hardcoded login and logout

---
+++ lib/actions.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+export async function logIn(email: string, password: string) {
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      return { success: false, error: "Invalid credentials" };
+    }
+
+    return { success: true };
+}
+
+export const logOut = async () => {
+  await signOut({ callbackUrl: "/login" });
+}
+
+export function checkAuth(status: string, redirectPath: string = "/login") {
+  if (status === "unauthenticated") {
+    redirect(redirectPath);
+  }
+}
